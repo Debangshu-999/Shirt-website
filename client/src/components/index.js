0a043456fcea8b5985fcfc1889fcbbb9c4d74c35
Debangshu-999 <debangshu.d001@gmail.com>
2024-12-20 14:10:57 +0530

    first

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import AiPicker from "./AiPicker"
+import ColorPicker from "./ColorPicker"
+import FilePicker from "./Filepicker"
+import Tab from "./Tab"
+
+export {CustomButton, AiPicker, ColorPicker, FilePicker, Tab}
